7361865313a9395339a284a5fece768473116bd0
violet-koki <yasumotoke_0808@yahoo.co.jp>
2025-01-06 17:52:42 +0900

    feat: firebaseの諸々設定

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,8 @@
+import { getDataConnect, validateArgs } from 'firebase/data-connect';
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'cosplayer_rank',
+  location: 'us-central1'
+};
+
